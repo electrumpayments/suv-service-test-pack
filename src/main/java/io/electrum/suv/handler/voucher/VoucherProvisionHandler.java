c122408b948e9c6879065ef9ddf9d77de64aa361
Luc Hayward <luchay10@gmail.com>
2018-11-27 08:39:15 +0200

    Working on VoucherProvisionHandler.handle()

--- src/main/java/io/electrum/suv/handler/voucher/VoucherProvisionHandler.java
+++ src/main/java/io/electrum/suv/handler/voucher/VoucherProvisionHandler.java
@@ -1,12 +1,19 @@
 package io.electrum.suv.handler.voucher;
 
+import io.electrum.suv.api.models.ErrorDetail;
 import io.electrum.suv.api.models.ProvisionRequest;
+import io.electrum.suv.api.models.ProvisionResponse;
 import io.electrum.suv.handler.BaseHandler;
+import io.electrum.suv.server.SUVTestServerRunner;
+import io.electrum.suv.server.util.RequestKey;
+import io.electrum.suv.server.util.VoucherModelUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class VoucherProvisionHandler extends BaseHandler {
    private static final Logger log = LoggerFactory.getLogger(VoucherProvisionHandler.class);
@@ -20,12 +27,75 @@ public class VoucherProvisionHandler extends BaseHandler {
       return "Voucher Provision";
    }
 
-   public Response handle(ProvisionRequest provisionRequest) {
-      // TODO Actually implement this method
+   public Response handle(ProvisionRequest provisionRequest, UriInfo uriInfo) {
+      // TODO Remove
+      log.info("Called handle on VoucherProvisionHandler");
 
-       //TODO Remove
-       log.info("Called handle on VoucherProvisionHandler");
+      try {
+         Response rsp;
+         // TODO Actually implement this method
+         // TODO Validate parameters are consistent and correct
+         if (!VoucherModelUtils.isUuidConsistent(provisionRequest.getId())) {
+            return Response.status(400).entity((buildVoucherRequestErrorResponse(provisionRequest.getId()))).build();
+         }
 
-       return null;
+         if (!provisionRequest.getClient().getId().equals(username)) {
+            return VoucherModelUtils.buildIncorrectUsernameErrorResponse(
+                  provisionRequest.getId(),
+                  provisionRequest.getClient(),
+                  username,
+                  ErrorDetail.ErrorType.AUTHENTICATION_ERROR);
+         }
+
+         // TODO canProvisionVoucher?
+         /*
+          * rsp = VoucherModelUtils.canProvisionVoucher(voucherId, username, password); if(rsp!=null){ return rsp; }
+          */
+
+         // todo fill method
+         RequestKey key = addVoucherRequestToCache(provisionRequest.getId(), provisionRequest);
+
+         ProvisionResponse provisionRsp = VoucherModelUtils.voucherRspFromReq(provisionRequest);
+
+         addVoucherResponseToCache(key, provisionRsp);
+
+         rsp = Response.created(uriInfo.getRequestUri()).entity(provisionRsp).build();
+
+         return rsp;
+
+      } catch (Exception e) {
+         return logAndBuildException(e);
+      }
+   }
+
+   private ErrorDetail buildVoucherRequestErrorResponse(String voucherId) {
+      // TODO Implement method
+      ErrorDetail errorDetail = new ErrorDetail();
+      return errorDetail;
+   }
+
+   // Todo confirm correct function of method
+   /**
+    * Adds the voucher request to the VoucherProvisionRecords
+    *
+    * @param voucherId
+    *           the unique identifier for this request
+    * @param request
+    *           the provision request to be recorded
+    * @return the corresponding {@link RequestKey} for this entry.
+    */
+   private RequestKey addVoucherRequestToCache(String voucherId, ProvisionRequest request) {
+      RequestKey key = new RequestKey(username, password, RequestKey.VOUCHERS_RESOURCE, voucherId);
+      ConcurrentHashMap<RequestKey, ProvisionRequest> provisionRecords =
+            SUVTestServerRunner.getTestServer().getVoucherProvisionRecords();
+      provisionRecords.put(key, request);
+      return key;
+   }
+
+   private void addVoucherResponseToCache(RequestKey key, ProvisionResponse provisionRsp) {
+      ConcurrentHashMap<RequestKey, ProvisionRequest> provisionRecords =
+            SUVTestServerRunner.getTestServer().getVoucherProvisionRecords();
+      provisionRecords.put(key, request);
+      return key;
    }
 }
--- src/main/java/io/electrum/suv/server/util/VoucherModelUtils.java
+++ src/main/java/io/electrum/suv/server/util/VoucherModelUtils.java
@@ -1,10 +1,81 @@
 package io.electrum.suv.server.util;
 
+import io.electrum.suv.api.models.ErrorDetail;
+import io.electrum.suv.api.models.ProvisionRequest;
+import io.electrum.suv.api.models.ProvisionResponse;
+import io.electrum.suv.server.model.DetailMessage;
+import io.electrum.vas.JsonUtil;
+import io.electrum.vas.model.Institution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 public class VoucherModelUtils extends SUVModelUtils {
    static Logger log = LoggerFactory.getLogger(VoucherModelUtils.class);
 
-   //TODO Implement class
+   // TODO Implement class
+
+   public static boolean isUuidConsistent(String id) {
+      // TODO validate uuid
+
+      return true;
+   }
+
+   // TODO Ensure method is correct
+   public static Response buildIncorrectUsernameErrorResponse(
+         String objectId,
+         Institution client,
+         String username,
+         ErrorDetail.ErrorType requestType) {
+
+      ErrorDetail errorDetail =
+            buildErrorDetail(
+                  objectId,
+                  "Incorrect username",
+                  "The HTTP Basic Authentication username (" + username
+                        + ") is not the same as the value in the Client.Id field (" + client.getId() + ").",
+                  null,
+
+                  ErrorDetail.ErrorType.FORMAT_ERROR);
+
+      DetailMessage detailMessage = (DetailMessage) errorDetail.getDetailMessage();
+      detailMessage.setClient(client);
+
+      return Response.status(400).entity(errorDetail).build();
+   }
+
+   // TODO Ensure method actually does what it should do
+   public static ErrorDetail buildErrorDetail(
+         String objectId,
+         String errorMessage,
+         String detailMessageFreeString,
+         String originalMsgId,
+         ErrorDetail.ErrorType errorType) {
+
+      ErrorDetail errorDetail =
+            new ErrorDetail().errorType(errorType).errorMessage(errorMessage).id(objectId).originalId(originalMsgId);
+
+      DetailMessage detailMessage = new DetailMessage();
+      detailMessage.setFreeString(detailMessageFreeString);
+      detailMessage.setReversalId(originalMsgId);
+
+      errorDetail.setDetailMessage(detailMessage);
+
+      return errorDetail;
+   }
+
+   // TODO Ensure method actually does what it should do
+   public static ProvisionResponse voucherRspFromReq(ProvisionRequest req) throws IOException {
+      ProvisionResponse voucherResponse =
+            JsonUtil.deserialize(JsonUtil.serialize(req, ProvisionRequest.class), ProvisionResponse.class);
+
+      updateWithRandomizedIdentifiers(voucherResponse);
+      voucherResponse.setVoucher(createRandomizedVoucher());
+      voucherResponse.setSlipData(createRandomizedSlipData());
+      voucherResponse.setResponseProduct(req.getProduct().name("TalkALot").type(Product.ProductType.AIRTIME_FIXED));
+
+      return voucherResponse;
+   }
 }
