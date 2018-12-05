8a75f311dbfee2680bfba7e041f8b7f66a6cef3b
Luc Hayward <luchay10@gmail.com>
2018-12-05 12:52:51 +0200

    Corrected Refunds Resources naming

---
+++ src/main/java/io/electrum/suv/resource/impl/RefundsResource.java
@@ -0,0 +1,28 @@
+package io.electrum.suv.resource.impl;
+
+import io.electrum.suv.api.IRefundsResource;
+import io.electrum.suv.api.utils.Paths;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.Authorization;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.Path;
+
+//TODO Can this not be generalized across the resource implementations
+//TODO Are PATHS necessary
+@Path(Paths.BASE_PATH + Paths.REFUND_VOUCHER)
+@Api(description = "the Single Use Voucher API", authorizations = { @Authorization("httpBasic") })
+public class RefundsResource extends io.electrum.suv.api.RefundsResource {
+   static RefundsResourceImpl instance = null;
+   private static final Logger log = LoggerFactory.getLogger(SUVTestServer.class.getPackage().getName());
+
+   @Override
+   protected IRefundsResource getResourceImplementation() {
+      if (instance == null) {
+         instance = new RefundsResourceImpl();
+      }
+      return instance;
+   }
+
+}
--- src/main/java/io/electrum/suv/resource/impl/RefundsResourceImpl.java
+++ src/main/java/io/electrum/suv/resource/impl/RefundsResourceImpl.java
@@ -1,29 +1,76 @@
 package io.electrum.suv.resource.impl;
 
 import io.electrum.suv.api.IRefundsResource;
-import io.electrum.suv.api.RefundsResource;
-import io.electrum.suv.api.utils.Paths;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.Authorization;
+import io.electrum.suv.api.models.RefundRequest;
+import io.electrum.suv.handler.SUVMessageHandlerFactory;
+import io.electrum.vas.model.BasicAdvice;
+import io.electrum.vas.model.BasicReversal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.Path;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.core.*;
 
-//TODO Can this not be generalized across the resource implementations
-//TODO Are PATHS necessary
-@Path(Paths.BASE_PATH + Paths.REFUND_VOUCHER)
-@Api(description = "the Single Use Voucher API", authorizations = { @Authorization("httpBasic") })
-public class RefundsResourceImpl extends RefundsResource {
-   static RefundsResourceInterfaceImpl instance = null;
+//TODO Implement methods correctly
+public class RefundsResourceImpl implements IRefundsResource {
    private static final Logger log = LoggerFactory.getLogger(SUVTestServer.class.getPackage().getName());
 
    @Override
-   protected IRefundsResource getResourceImplementation() {
-      if (instance == null) {
-         instance = new RefundsResourceInterfaceImpl();
-      }
-      return instance;
+   public void confirmRefund(
+         BasicAdvice body,
+         SecurityContext securityContext,
+         Request request,
+         AsyncResponse asyncResponse,
+         HttpHeaders httpHeaders,
+         UriInfo uriInfo,
+         HttpServletRequest httpServletRequest) {
+      log.info(String.format("%s %s", httpServletRequest.getMethod(), uriInfo.getPath()));
+      log.debug(String.format("%s %s\n%s", httpServletRequest.getMethod(), uriInfo.getPath(), body));
+      Response rsp =
+            SUVMessageHandlerFactory.getRefundConfirmationHandler(httpHeaders)
+                  .handle(/* requestId, confirmationId, */ body, uriInfo);
+      log.debug(String.format("Entity returned:\n%s", rsp.getEntity()));
+
+      asyncResponse.resume(rsp);
+
    }
 
+   @Override
+   public void reverseRefund(
+         BasicReversal body,
+         SecurityContext securityContext,
+         Request request,
+         AsyncResponse asyncResponse,
+         HttpHeaders httpHeaders,
+         UriInfo uriInfo,
+         HttpServletRequest httpServletRequest) {
+      log.info(String.format("%s %s", httpServletRequest.getMethod(), uriInfo.getPath()));
+      log.debug(String.format("%s %s\n%s", httpServletRequest.getMethod(), uriInfo.getPath(), body));
+      Response rsp =
+            SUVMessageHandlerFactory.getRefundReversalHandler(httpHeaders)
+                  .handle(/* requestId, confirmationId, */ body, uriInfo);
+      log.debug(String.format("Entity returned:\n%s", rsp.getEntity()));
+
+      asyncResponse.resume(rsp);
+   }
+
+   @Override
+   public void refundVoucher(
+         RefundRequest body,
+         SecurityContext securityContext,
+         Request request,
+         AsyncResponse asyncResponse,
+         HttpHeaders httpHeaders,
+         UriInfo uriInfo,
+         HttpServletRequest httpServletRequest) {
+      log.info(String.format("%s %s", httpServletRequest.getMethod(), uriInfo.getPath()));
+      log.debug(String.format("%s %s\n%s", httpServletRequest.getMethod(), uriInfo.getPath(), body));
+      Response rsp =
+            SUVMessageHandlerFactory.getRefundVoucherHandler(httpHeaders)
+                  .handle(/* requestId, confirmationId, */ body, uriInfo);
+      log.debug(String.format("Entity returned:\n%s", rsp.getEntity()));
+
+      asyncResponse.resume(rsp);
+   }
 }
